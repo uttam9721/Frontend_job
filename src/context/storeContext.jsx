dc38d5f9409cf8bc41479dd572a4eff4e090a01a
Uttam Kumar <uttammaurya377@gmail.com>
2024-12-31 01:55:16 +0530

    job

---
+++ src/context/storeContext.jsx
@@ -0,0 +1,100 @@
+// import { createContext, useEffect, useState } from "react";
+// // import { StoreContext } from './storeContext';
+// import { food_list } from "../assets/assets";
+
+// export const StoreContext = createContext(null)
+
+// const StoreContextProvider = (props)=>{
+
+//     const {cartItems,setCartItems}=useState({})
+
+// // AddToCart 
+
+//     const addToCart =(itemId)=>{
+//         if(!cartItems[itemId]){
+//             setCartItems((prev)=>({...prev,[itemId]:1}))
+//         }else{
+//             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+//         }
+//     }
+
+//     // remove to cart
+//     const removeFromCart = (itemId) => {
+//             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+//         }
+        
+// useEffect(()=>{
+//     console.log(cartItems);
+//     },[cartItems])
+//     const contextValue ={
+//         food_list,
+//         cartItems,
+//         setCartItems,
+//         addToCart,
+//         removeFromCart
+        
+//     }
+//     return(
+//         <StoreContext.Provider value={contextValue}>
+//             {props.children}
+//         </StoreContext.Provider>
+//     )
+// }
+
+// export default StoreContextProvider;
+
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({}); // Corrected syntax
+
+  // Add to Cart
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  // Remove from Cart
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const updatedCart = { ...prev, [itemId]: prev[itemId] - 1 };
+      if (updatedCart[itemId] <= 0) delete updatedCart[itemId];
+      return updatedCart;
+    });
+  };
+
+  const getTotalCartAmount=()=>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        // totalAmount += food_list[item].price * cartItems[item];
+        let itemInfo = food_list.find((product)=>product._id===item)
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
